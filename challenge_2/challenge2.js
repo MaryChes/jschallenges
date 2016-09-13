44188a25f568d8cbe192a6e616044d24e56dc436
MaryChes <chessie4907@gmail.com>
2016-09-13 16:55:13 -0400

    Almost done challenge 2 just need to remove last two posts from middle column

--- challenge_2/challenge2.js
+++ challenge_2/challenge2.js
@@ -1,5 +1,42 @@
 $(function(){
+var control = 0;
 
-	//all your code should go here
+/*
+* Changes font color from black to red and back
+*/
+$('h1').click(function(){
+	if (control ==0){
+		$('h1').css("color", "red");
+		control++;
+	}else{
+		$('h1').css("color", "black");
+		control--;
+	}
+});
+
+
+/*
+* Hides odd sections
+*/
+$('.title:even').hide();
+$('footer li:even').hide();
+
+
+/*
+* Replace every instance of "bacon" with "Laser Vision"
+*/
+$('p').each(function() {
+    var text = $(this).text();
+    $(this).text(text.replace('Bacon', 'Laser Vision')); 
+});
+
+
+
+/*
+* Removes pictures on aside
+*/
+$('aside').remove();
+
+
 
 })
--- challenge_2_alt/challenge2_alt.js
+++ challenge_2_alt/challenge2_alt.js
@@ -30,6 +30,91 @@
 
  $(function(){
 
+ 	var shootButtonOne = $('#teamone-shoot');
+ 	var shootButtonTwo = $('#teamtwo-shoot');
+ 	var resetButton = $('#reset');
+ 	var counter1 = 0;
+ 	var counter2 = 0;
+ 	var resetCounter = 0;
+ 	var teamOneHits = 0;
+ 	var teamTwoHits = 0;
 
+/*
+*
+*Generates 0 or 1 randomly 
+*0=Miss 1=Goal
+*
+*/
+ 	function getRandomIntInclusive() {
+  	min = Math.ceil(0);
+  	max = Math.floor(1);
+  	return Math.floor(Math.random() * (max - min + 1)) + min;
+	};
+
+/*
+*
+*Controls Team 1 functionality, updates counters, changes color when goal is made
+*
+*
+*/
+ 	shootButtonOne.click(function(){
+ 		counter1++;
+ 		$('#teamone-numshots').html(counter1);
+ 		var result = getRandomIntInclusive();
+
+ 		if (result == 1){
+ 			$('body').css("background-color","purple");
+ 			alert("SCORE!!!!!!!!!!!!!!!!!!!!!")
+ 			teamOneHits++;
+ 			$('#teamone-numhits').html(teamOneHits)
+ 		}else{
+ 			$('body').css("background-color","white");
+ 			alert("Miss!!!!!!!!!!")
+ 		}
+ 	});
+
+/*
+*
+*Controls Team 2 functionality, updates counters, changes color when goal is made
+*
+*
+*/
+
+ 	 	shootButtonTwo.click(function(){
+ 		counter2++;
+ 		$('#teamtwo-numshots').html(counter2);
+ 		var result = getRandomIntInclusive();
+
+ 		if (result == 1){
+ 			$('body').css("background-color","orange");
+ 			alert("SCORE!!!!!!!!!!!!!!!!!!!!!")
+ 			teamTwoHits++;
+ 			$('#teamtwo-numhits').html(teamTwoHits)
+ 		}else{
+ 			$('body').css("background-color","white");
+ 			alert("Miss!!!!!!!!!!")
+ 		}
+ 	});
+ 	
+/*
+*
+*Controls Reset functionality, updates reset counter, clears content for teams 1 and 2
+*
+*/
+ 	 	resetButton.click(function(){
+ 	 		$('body').css("background-color","white");
+ 	 		resetCounter++;
+ 	 		counter1 = 0;
+ 			counter2 = 0;
+ 			teamOneHits = 0;
+ 			teamTwoHits = 0;
+ 			$('#num-resets').html(resetCounter)
+ 			$('#teamone-numshots').html(counter1);
+ 			$('#teamone-numhits').html(teamOneHits);
+ 			$('#teamtwo-numshots').html(counter2);
+ 			$('#teamtwo-numhits').html(teamTwoHits);
+
+ 	 	})
 
  })
+
